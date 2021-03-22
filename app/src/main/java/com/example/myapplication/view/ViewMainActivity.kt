9bea0696a7e16a650a6731506493ad737fd9961f
h_bagheri <hassanbagheri233@gmail.com>
2021-03-22 22:20:57 +0430

    add dilog call me

--- app/src/main/java/com/example/myapplication/view/ViewMainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/ViewMainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication.view
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.graphics.ColorSpace
+import android.net.Uri
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.PopupMenu
+import androidx.appcompat.widget.AlertDialogLayout
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
@@ -18,6 +23,7 @@ import com.example.myapplication.utility.Utility
 import com.google.android.material.bottomnavigation.BottomNavigationMenu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.dialog_call_me.view.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
@@ -83,32 +89,49 @@ class ViewMainActivity(
     }
 
 
-
     override fun onMenuItemClick(item: MenuItem?): Boolean {
-    return when(item?.itemId){
-        R.id.menu_main_porsesh -> {
+        return when (item?.itemId) {
+            R.id.menu_main_porsesh -> {
 //            context.toast("پرسش های متداول")
-            context.startActivity<questionActivity>()
-            true
-        }
-        R.id.menu_main_about -> {
+                context.startActivity<questionActivity>()
+                true
+            }
+            R.id.menu_main_about -> {
 //            context.toast("درباره ما")
-            context.startActivity<AboutMeActivity>()
-            true
+                context.startActivity<AboutMeActivity>()
+                true
+            }
+            R.id.menu_main_call -> {
+//
+//            context.toast("تماس باما")
+                val view = LayoutInflater.from(context).inflate(R.layout.dialog_call_me, null)
+                val dialog = AlertDialog.Builder(context)
+
+                dialog.setView(view)
+                dialog.show()
+
+                view.txt_call_me.setOnClickListener {
+
+                    val intent = Intent(Intent.ACTION_DIAL)
+                    intent.data = Uri.parse("tel:09194183978")
+                    if (intent.resolveActivity(context.packageManager) != null)
+                        context.startActivity(intent)
+
+                }
+                true
+            }
+            R.id.menu_main_error -> {
+                context.toast("'گزارش خطا")
+                true
+            }
+            else -> false
         }
-        R.id.menu_main_call -> {context.toast("تماس باما")
-            true}
-        R.id.menu_main_error -> {
-            context.toast("'گزارش خطا")
-            true
-        }else -> false
-    }
     }
 
 
-    fun menuup(){
-        val popup = PopupMenu(context,menu)
-        popup.menuInflater.inflate(R.menu.menu_main,popup.menu)
+    fun menuup() {
+        val popup = PopupMenu(context, menu)
+        popup.menuInflater.inflate(R.menu.menu_main, popup.menu)
         popup.setOnMenuItemClickListener(this)
         popup.show()
     }
