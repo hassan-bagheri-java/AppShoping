b5f2e81829b933aba51936121515b967805f4750
hassanbagheri-developer <h.bagheri@gmail.com>
2021-04-18 12:50:27 +0430

    add color telgram

--- app/src/main/java/com/example/myapplication/androidWrapper/App.kt
+++ app/src/main/java/com/example/myapplication/androidWrapper/App.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.androidWrapper
 import android.app.Application
 import com.example.myapplication.di.*
 import org.koin.android.ext.android.startKoin
+import org.koin.standalone.StandAloneContext.startKoin
 
 class App : Application() {
 
