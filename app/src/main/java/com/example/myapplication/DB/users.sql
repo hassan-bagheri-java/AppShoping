74be8ffd35a968da152ae06124afb78a85c0ba60
h_bagheri <hassanbagheri233@gmail.com>
2021-04-25 17:35:43 +0430

    add prosejer

--- app/src/main/java/com/example/myapplication/DB/users.sql
+++ app/src/main/java/com/example/myapplication/DB/users.sql
@@ -18,7 +18,11 @@ INSERT INTO `users` (`email`, `password`) VALUES ('hassan1@gmail.com', '123456')
 SELECT * FROM users
 WHERE email IN ('hassan1@gmail.com') AND password IN ('123456');
 
-
+SELECT count(users.email) FROM users
+WHERE users.email COLLATE utf8_unicode_ci = email
+AND
+users.password COLLATE utf8_unicode_ci = pass
+;
 
 
 DROP PROCEDURE IF EXISTS `check_login`;
