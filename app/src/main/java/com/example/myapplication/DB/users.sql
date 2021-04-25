fad1673cd0111337d33332eb9b3e2b726e01886b
h_bagheri <hassanbagheri233@gmail.com>
2021-04-25 11:02:34 +0430

    add PROCEDURE :)))) 1400/02/05

--- app/src/main/java/com/example/myapplication/DB/users.sql
+++ app/src/main/java/com/example/myapplication/DB/users.sql
@@ -17,3 +17,19 @@ INSERT INTO `users` (`email`, `password`) VALUES ('hassan1@gmail.com', '123456')
 
 SELECT * FROM users
 WHERE email IN ('hassan1@gmail.com') AND password IN ('123456');
+
+
+
+
+DROP PROCEDURE IF EXISTS `check_login`;
+
+CREATE PROCEDURE `check_login`(IN email VARCHAR(200) , IN pass VARCHAR(200))
+BEGIN
+  SELECT * FROM users
+  WHERE users.email COLLATE utf8_unicode_ci = email
+	AND
+	users.password COLLATE utf8_unicode_ci = pass
+	;
+END;
+
+CALL check_login('hassan1@gmail.com','123456')
